874d5a01ca0b890589db97cd3f8977503e548646
Mohit <joshimohit8130@gmail.com>
2025-07-07 09:46:10 +0530

    Update coinChartController.js

--- backend/controllers/coinChartController.js
+++ backend/controllers/coinChartController.js
@@ -7,21 +7,21 @@ const CACHE_DURATION = 10 * 60 * 1000; // 10 minutes
 
 export const getCoinChart = async (req, res) => {
   try {
-    const { coinId, days } = req.query; // ?coinId=bitcoin&days=30
+    const { coinId, days } = req.query;  
     const cacheKey = `${coinId}_${days}`;
     const now = Date.now();
 
     if (cache.has(cacheKey)) {
       const { timestamp, data } = cache.get(cacheKey);
       if (now - timestamp < CACHE_DURATION) {
-        return res.json(data); // ✅ Return cached data
+        return res.json(data);  
       }
     }
 
     const url = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=inr&days=${days}`;
     const response = await axios.get(url);
 
-    // ✅ Update cache
+     
     cache.set(cacheKey, {
       timestamp: now,
       data: response.data,
